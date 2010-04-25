b201487e22abfdfde8bcad970790cea8eeadbfc7
markcohen <macohendev@gmail.com>
2010-04-25 05:25:49 -0500

    Fix for renamed mainForm

--- GitTest/src/Program.cs
+++ GitTest/src/Program.cs
@@ -13,7 +13,7 @@ namespace GitTest
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
