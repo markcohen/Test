2ff673eb5412d2d182f482950d084737b8cedf5a
markcohen <macohendev@gmail.com>
2010-04-25 05:23:58 -0500

    Renamed Form1 to MainForm

---
+++ GitTest/src/MainForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GitTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+    }
+}
