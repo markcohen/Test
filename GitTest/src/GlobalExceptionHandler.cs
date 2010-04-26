c26f4f9e3016754243d4fc016cac665f344549be
markcohen <macohendev@gmail.com>
2010-04-25 20:10:30 -0500

    Simplified global exception handler class

--- GitTest/src/GlobalExceptionHandler.cs
+++ GitTest/src/GlobalExceptionHandler.cs
@@ -16,17 +16,9 @@ namespace GitTest
             }
             catch
             {
-                try
-                {
-                    MessageBox.Show("Fatal Windows Forms Error", "Fatal Windows Forms Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                }
-                finally
-                {
-                    Application.Exit();
-                }
+                MessageBox.Show("Fatal Error", "Fatal Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
             }
 
-            // Exits the program when the user clicks Abort.
             if (result == DialogResult.Abort)
             {
                 Application.Exit();
@@ -38,9 +30,8 @@ namespace GitTest
         {
             try
             {
-                var ex = (Exception)e.ExceptionObject;
-                string errorMsg = "The following fatal application error occurred:\n\n";
-                errorMsg = errorMsg + ex.Message + "\n\nStack Trace:\n" + ex.StackTrace;
+                var ex = (Exception) e.ExceptionObject;
+                string errorMsg = FormatErrorMessage(ex);
 
                 // Since we can't prevent the app from terminating, log this to the event log.
                 if (!EventLog.SourceExists("ThreadException"))
@@ -49,31 +40,35 @@ namespace GitTest
                 }
 
                 // Create an EventLog instance and assign its source.
-                var myLog = new EventLog {Source = "ThreadException"};
-                myLog.WriteEntry(errorMsg);
+                new EventLog { Source = "ThreadException" }.WriteEntry(errorMsg);
             }
             catch (Exception exc)
             {
-                try
-                {
-                    MessageBox.Show("Could not write the error to the event log. Reason:\n\n" + exc.Message, 
-                        "Fatal Error",
-                        MessageBoxButtons.OK, MessageBoxIcon.Stop);
-                }
-                finally
-                {
-                    Application.Exit();
-                }
+                MessageBox.Show("Could not write the error to the event log. Reason:\n\n" + exc.Message,
+                                "Fatal Error",
+                                MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            finally
+            {
+                Application.Exit();
             }
         }
 
         // Creates the error message and displays it.
         private static DialogResult ShowThreadExceptionDialog(string title, Exception e)
         {
-            string errorMsg = "The following application error occurred:\n\n";
-            errorMsg = errorMsg + e.Message + "\n\nStack Trace:\n" + e.GetType().Name + e.StackTrace;
+            string errorMsg = FormatErrorMessage(e);
 
             return MessageBox.Show(errorMsg, title, MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Stop);
+        }
+
+        private static string FormatErrorMessage(Exception e)
+        {
+            string errorMsg = string.Format(
+                "The following application error occurred:\n\n{0}\n\nStack Trace:\n{1}{2}", 
+                e.Message, e.GetType().Name, e.StackTrace);
+
+            return errorMsg;
         }
     }
 }
